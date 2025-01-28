dc9bba3c909a6df8768a508634f8d6a13172681f
lPitecus <tuzaum.silva@gmail.com>
2025-01-28 00:31:47 -0300

    Add models

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    related_project = models.ForeignKey(Project, on_delete=models.CASCADE)
