ee54f3171b17beb290467d4b27b28ec2e41acb6e
lPitecus <tuzaum.silva@gmail.com>
2025-01-29 00:44:24 -0300

    Add name method to models.py

--- projects/models.py
+++ projects/models.py
@@ -5,6 +5,8 @@ class Project(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
@@ -12,3 +14,5 @@ class Task(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     related_project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
