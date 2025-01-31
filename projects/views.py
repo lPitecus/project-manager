3c60d919bd1f0bcc1288f49365432885146c367d
lPitecus <tuzaum.silva@gmail.com>
2025-01-30 23:44:31 -0300

    Add 404 error if the project or task is not found

--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from .models import Project, Task
 
 # Create your views here.
@@ -10,7 +9,7 @@ def index(request):
 
 
 def project(request, project_id):
-    current_project = Project.objects.get(pk=project_id)
+    current_project = get_object_or_404(Project, pk=project_id)
     project_tasks = Task.objects.filter(related_project=current_project)
     context = {
         "current_project": current_project,
@@ -20,8 +19,8 @@ def project(request, project_id):
 
 
 def task(request, project_id, task_id):
-    current_task = Task.objects.get(pk=task_id)
-    current_project = Project.objects.get(pk=project_id)
+    current_task = get_object_or_404(Task, pk=task_id)
+    current_project = get_object_or_404(Project, pk=project_id)
     context = {
         "current_task": current_task,
         "current_project": current_project,
