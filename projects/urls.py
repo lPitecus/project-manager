75edcfe593764ab18d9f794559f454da4fb04fb1
lPitecus <tuzaum.silva@gmail.com>
2025-01-30 23:59:32 -0300

    Namespace URL names

--- projects/urls.py
+++ projects/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+# Essa variável diz a qual app pertence a url dinâmica nos templates
+# https://docs.djangoproject.com/en/5.1/intro/tutorial03/#namespacing-url-names
+app_name = "projects"
 # Essa lista deve conter as rotas para as funcoes definidas em projects/views.py
 urlpatterns = [
     path('', views.index, name='index'),
