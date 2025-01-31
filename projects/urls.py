8d2b67c234baf861b83b0afbe6bf4c2f496e3490
lPitecus <tuzaum.silva@gmail.com>
2025-01-30 22:05:57 -0300

    Add index and projects views

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,5 @@ from . import views
 # Essa lista deve conter as rotas para as funcoes definidas em projects/views.py
 urlpatterns = [
     path('', views.index, name='index'),
-    path('home/', views.home, name='home')
+    path('project/<int:project_id>', views.project, name='project')
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project, Task
 
 # Create your views here.
 def index(request):
-    return HttpResponse("na verdade")
+    lista_projetos = Project.objects.order_by("-created_at")
+    context = {"lista_projetos": lista_projetos}
+    return render(request, "projects/index.html", context)
 
 
-def home(request):
-    return HttpResponse("Chegou aqui pq arrombado kkkkkkkkkk")
+def project(request, project_id):
+    current_project = Project.objects.get(pk=project_id)
+    context = {"current_project": current_project}
+    return render(request, "projects/project.html", context)
