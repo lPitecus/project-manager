73fa641436e9af4165c3b7017cd56a0480fa53d3
lPitecus <tuzaum.silva@gmail.com>
2025-02-05 20:00:38 -0300

    Enhance admin and add project image to template
    Introduced custom admin classes for Project and Task models to improve admin display. Added a new static image to the project template, enhancing the visual layout with a themed graphic.

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import Project, Task
 
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["name", "related_project", "created_at"]
+
+
 # Register your models here.
-admin.site.register(Project)
-admin.site.register(Task)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
