aa2876c78686303776c207811f1aeb1d14ac3c8b
lPitecus <tuzaum.silva@gmail.com>
2025-02-06 21:45:37 -0300

    Enhance admin panel for projects and tasks

--- projects/admin.py
+++ projects/admin.py
@@ -1,15 +1,21 @@
 from django.contrib import admin
-from django.utils.html import format_html
-from django.urls import reverse
 from .models import Project, Task
+
+class TaskInline(admin.StackedInline):
+    model = Task
+    extra = 0
+
 
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ["name", "created_at"]
+    inlines = [TaskInline]
 
 
 class TaskAdmin(admin.ModelAdmin):
     list_display = ["name", "related_project", "created_at"]
-
+    search_fields = ["name"]
+    list_editable = ["related_project"]
+    list_filter = ["related_project"]
 
 # Register your models here.
 admin.site.register(Project, ProjectAdmin)
