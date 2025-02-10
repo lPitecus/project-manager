ea5238adc1ed01016b2690a5c0226addb9a2e6d5
lPitecus <tuzaum.silva@gmail.com>
2025-02-09 22:57:24 -0300

    Add admin interface for Profile model
    Introduce a custom admin class for the Profile model with list display, search fields, and filters for easier management in the admin panel. Register the Profile model with this custom admin configuration.

--- users/admin.py
+++ users/admin.py
@@ -1,1 +1,14 @@
 # Register your models here.
+from django.contrib import admin
+
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user__username", "user__email", "gender", "birth_date", "user__is_superuser"]
+    search_fields = ["user__username", "user__email"]
+    list_filter = ["gender"]
+
+
+# Register your models here.
+admin.site.register(Profile, ProfileAdmin)
