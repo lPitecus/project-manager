4dc8645f6d7e09a96819b40b932c4c19ccaf3b8e
lPitecus <tuzaum.silva@gmail.com>
2025-03-11 21:34:30 -0300

    Iniciar criação de tela de registro e login

--- users/models.py
+++ users/models.py
@@ -5,4 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     birth_date = models.DateField(null=True, blank=True)
-    
+
+    def __str__(self):
+        return f"Perfil de {self.user.username}"
